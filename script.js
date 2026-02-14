974087f7676c17ce8cb0b5ccb4f2075c273d3ebb
Colin Keogh <colin-182@hotmail.com>
2026-02-14 21:13:13 +0000

    JS added
    java started;
    - consts for form + DOM elements

---
+++ script.js
@@ -0,0 +1,43 @@
+// Workout state
+let workouts = [];
+let weeklyGoal = 3;
+
+// DOM elements
+const getStartedBtn = document.getElementById('get-started-btn');
+
+const workoutForm = document.getElementById('workout-form');
+const formMessages = document.getElementById('form-messages');
+
+const dateInput = document.getElementById('workout-date');
+const typeInput = document.getElementById('workout-type');
+const durationInput = document.getElementById('duration');
+const intensityInput = document.getElementById('intensity');
+const notesInput = document.getElementById('notes');
+
+const dateError = document.getElementById('date-error');
+const typeError = document.getElementById('type-error');
+const durationError = document.getElementById('duration-error');
+
+const historyBody = document.getElementById('history-body');
+const filterButtons = document.getElementById('.filter-btn');
+
+const totalWorkoutsEl = document.getElementById('total-workouts');
+const totalMinutesEl = document.getElementById('total-minutes');
+const weeklyWorkoutsEl = documents.getElementById('weekly-workouts');
+
+const weeklyGoalInput = document.getElementById('weekly-goal');
+const saveGoalBtn = document.getElementById('save-goal-btn');
+
+// clearing forms & validations
+function clearFormErrors() {
+    dateError.textContent = '';
+    typeError.textContent = '';
+    durationError.textContent = '';
+    formMessages.textContent = '';
+    formMessages.className = 'form-messages';
+}
+
+function showFormMessage(message, type = 'error') {
+  formMessages.textContent = message;
+  formMessages.className = `form-messages ${type}`;
+}
